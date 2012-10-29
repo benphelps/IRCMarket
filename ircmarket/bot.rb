b5e019a428b877277756134c8cac73ecc4314ddd
Ben Phelps <bphelpsen@gmail.com>
2012-10-29 00:53:43 -0500

    force soft tabs

--- ircmarket/bot.rb
+++ ircmarket/bot.rb
@@ -5,19 +5,21 @@ require_relative 'database/database'
 # Plugins
 require_relative 'plugins/core/buy_long'
 
+Cinch::Plugin.prefix = /^./
+
 bot = Cinch::Bot.new do
-	configure do |c|
-		
-		c.server = "vps.benphelps.me"
-		c.port = 2390
-		c.password = "brokerdev:brokerdev"
-		c.channels = ["#phelps"]
-		c.messages_per_second = 999999
-		c.max_messages = 999999
-		c.plugins.plugins = [
-			BuyLong
-		]
-	end
+  configure do |c|
+    
+    c.server = "vps.benphelps.me"
+    c.port = 2390
+    c.password = "brokerdev:brokerdev"
+    c.channels = ["#phelps"]
+    c.messages_per_second = 999999
+    c.max_messages = 999999
+    c.plugins.plugins = [
+      BuyLong
+    ]
+  end
 
 end
 
